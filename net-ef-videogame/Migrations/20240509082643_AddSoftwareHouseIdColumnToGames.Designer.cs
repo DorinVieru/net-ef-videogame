53d3182b912f1a996fbf7c65ba44ffe2b067a3a6
DorinVieru <dorino.vieru@gmail.com>
2024-05-09 10:31:33 +0200

    UP: Upload new migration with correction

--- net-ef-videogame/Migrations/20240509082643_AddSoftwareHouseIdColumnToGames.Designer.cs
+++ net-ef-videogame/Migrations/20240509082643_AddSoftwareHouseIdColumnToGames.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using net_ef_videogame;
 
@@ -11,9 +12,11 @@ using net_ef_videogame;
 namespace net_ef_videogame.Migrations
 {
     [DbContext(typeof(GamesContext))]
-    partial class GamesContextModelSnapshot : ModelSnapshot
+    [Migration("20240509082643_AddSoftwareHouseIdColumnToGames")]
+    partial class AddSoftwareHouseIdColumnToGames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ net-ef-videogame/Migrations/20240509082643_AddSoftwareHouseIdColumnToGames.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace net_ef_videogame.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSoftwareHouseIdColumnToGames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "software_house_id",
+                table: "games",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_games_software_house_id",
+                table: "games",
+                column: "software_house_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_games_software_house_software_house_id",
+                table: "games",
+                column: "software_house_id",
+                principalTable: "software_house",
+                principalColumn: "SoftwareHouseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_games_software_house_software_house_id",
+                table: "games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_games_software_house_id",
+                table: "games");
+
+            migrationBuilder.DropColumn(
+                name: "software_house_id",
+                table: "games");
+        }
+    }
+}
